a245df6e734c662d32ae32efbdb58d1ddf2eaa7f
kosmosworkforcect <chad.till@kosmosworkforce.com>
2026-05-04 11:22:45 -0500

    Add files via upload

--- nav.js
+++ nav.js
@@ -7,8 +7,8 @@ class Navmenu extends HTMLElement {
 
 	<!-- LEFT NAVIGATION -->
 	<div class="nav-left">
-		<div onclick="window.location.href='individual.html'"><img src="images/KosmosWorkforceLogoWhite.png" width="184px" height="auto"></div>
-		<div data-link="index.html" onclick="window.location.href='individual.html'">
+		<div onclick="window.location.href='index.html'"><img src="images/KosmosWorkforceLogoWhite.png" width="184px" height="auto"></div>
+		<div data-link="index.html" onclick="window.location.href='index.html'">
 			<svg xmlns="http://www.w3.org/2000/svg" 
 				width="24" height="24" viewBox="0 0 24 24" fill="none"
 				stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
